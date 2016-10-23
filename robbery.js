9931a89a3f4dd22bbe5fee62bfd48d07fd6510e1
Колиниченко Александр <7e0d4547d5e940b9@naumen.ru>
2016-10-23 19:50:08 +0500

    first try

--- robbery.js
+++ robbery.js
@@ -4,7 +4,97 @@
  * Сделано задание на звездочку
  * Реализовано оба метода и tryLater
  */
+
 exports.isStar = true;
+
+
+var SEC_IN_MIN = 60;
+var SUNDAY = 'Sun, 4 Jan 1970 ';
+var MONDAY = 'Mon, 5 Jan 1970 ';
+var TUESDAY = 'Tue, 6 Jan 1970 ';
+var WEDNESDAY = 'Wed, 7 Jan 1970 ';
+var THURSDAY = 'Thu, 8 Jan 1970 ';
+var WORK_DAYS = {
+    'ВС': SUNDAY,
+    'ПН': MONDAY,
+    'ВТ': TUESDAY,
+    'СР': WEDNESDAY,
+    'ЧТ': THURSDAY
+};
+var GANGSTERS = ['Danny', 'Rusty', 'Linus'];
+
+function extractDay(timeString) {
+    var reg = /ВС|ПН|ВТ|СР|ЧТ/g;
+    var day = reg.exec(timeString)[0];
+
+    return WORK_DAYS[day];
+}
+
+
+function asTimeStamp(timeString, day) {
+    if (day === undefined) {
+        day = extractDay(timeString);
+    }
+
+    var reg = /([0-2][0-9]):([0-5][0-9])\+(\d+)/g;
+    var time = reg.exec(timeString);
+    var zone = time[3];
+
+    if (zone.length === 1) {
+        zone = '0' + zone;
+    }
+
+    var stdTimeString = day + time[1] + ':' + time[2] + ':00 +' + zone + '00';
+
+    return Date.parse(stdTimeString);
+}
+
+function overlap(rangeA, rangeB) {
+
+    return (rangeA[0] < rangeB[1]) && (rangeB[0] < rangeA[1]);
+}
+
+function bankDailyInterval(day, entry) {
+
+    return [asTimeStamp(entry.from, day), asTimeStamp(entry.to, day)];
+}
+
+function gangsterInterval(entry) {
+
+    return [asTimeStamp(entry.from), asTimeStamp(entry.to)];
+}
+
+function scheduleFitting(interval, gangsterSchedule) {
+
+    for (var i = 0; i < gangsterSchedule.length; i++) {
+        if (overlap(gangsterInterval(gangsterSchedule[i]), interval)) {
+
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function goForIt(interval, schedule) {
+    for (var i = 0; i < Object.keys(schedule).length; i++) {
+        if (!scheduleFitting(interval, schedule[GANGSTERS[i]])) {
+
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function zeroPadded(number) {
+    if (number < 10) {
+
+        return '0' + number;
+    }
+
+    return String(number);
+}
 
 /**
  * @param {Object} schedule – Расписание Банды
@@ -17,6 +107,48 @@ exports.isStar = true;
 exports.getAppropriateMoment = function (schedule, duration, workingHours) {
     console.info(schedule, duration, workingHours);
 
+    var timesForTheJob = [];
+    if (findTime()) {
+        timesForTheJob = findTime();
+    }
+
+    // ищет подходящее время для начала операции в указанный день
+    function findTodayTime(day) {
+        var suitableTimes = [];
+        var bankWorkHours = bankDailyInterval(day, workingHours);
+        var operationStart = bankWorkHours[0];
+        var operationEnd = operationStart + duration * SEC_IN_MIN * 1000;
+        var dailyDeadline = bankWorkHours[1];
+        while (operationEnd <= dailyDeadline) {
+            if (goForIt([operationStart, operationEnd], schedule)) {
+
+                suitableTimes.push(operationStart);
+            }
+            operationStart += SEC_IN_MIN * 1000;
+            operationEnd += SEC_IN_MIN * 1000;
+        }
+
+        return suitableTimes;
+    }
+
+    function findTime() {
+        var suitableTimes = [];
+        var day;
+        var days = Object.keys(WORK_DAYS);
+        for (var i = 1; i < 4; i++) {
+            day = WORK_DAYS[days[i]];
+            suitableTimes = suitableTimes.concat(findTodayTime(day));
+        }
+
+        if (suitableTimes.length > 0) {
+
+            return suitableTimes;
+        }
+
+        return false;
+    }
+
+
     return {
 
         /**
@@ -24,7 +156,7 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
          * @returns {Boolean}
          */
         exists: function () {
-            return false;
+            return (timesForTheJob.length > 0);
         },
 
         /**
@@ -35,7 +167,20 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
          * @returns {String}
          */
         format: function (template) {
-            return template;
+            if (!this.exists()) {
+
+                return '';
+            }
+            var stampGoTime = timesForTheJob[0];
+            var goTime = new Date(stampGoTime);
+            var hh = goTime.getHours();
+            var mm = goTime.getMinutes();
+            var dd = Object.keys(WORK_DAYS)[goTime.getDay()];
+
+            return template
+                .replace('%HH', zeroPadded(hh))
+                .replace('%MM', zeroPadded(mm))
+                .replace('%DD', dd);
         },
 
         /**
@@ -44,7 +189,22 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
          * @returns {Boolean}
          */
         tryLater: function () {
+            if (!this.exists() || timesForTheJob.length < 2) {
+
+                return false;
+            }
+
+            var difference = 30 * SEC_IN_MIN * 1000;
+            for (var i = 0; i < timesForTheJob.length; i++) {
+                if (timesForTheJob[i] - timesForTheJob[0] >= difference) {
+                    timesForTheJob.splice(0, i);
+
+                    return true;
+                }
+            }
+
             return false;
+
         }
     };
 };
